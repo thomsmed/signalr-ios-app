2b2c7274323c97ffafa9e3a6fc483f955a732eea
Thomas Asheim Smedmann <thomsmed89@gmail.com>
2020-03-14 21:46:31 +0100

    Initial Commit

---
+++ signalr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signalr
+//
+//  Created by thomsmed on 14/03/2020.
+//  Copyright © 2020 Thomas A. Smedmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
